79059d2e3bdd77de3fab5baa8ba56a49cf58208a
MingHieu <hieubeo0612@gmail.com>
2024-03-11 01:09:00 +0700

    add BookmarkWorkoutPlanFragment

--- app/src/main/java/com/ltmb/fitness/scene/bookmarkworkoutplan/BookmarkWorkoutPlanFragment.kt
+++ app/src/main/java/com/ltmb/fitness/scene/bookmarkworkoutplan/BookmarkWorkoutPlanFragment.kt
@@ -3,6 +3,7 @@ package com.ltmb.fitness.scene.bookmarkworkoutplan
 import com.ltmb.fitness.R
 import com.ltmb.fitness.base.BaseFragment
 import com.ltmb.fitness.databinding.FragmentBookmarkWorkoutPlanBinding
+import com.ltmb.fitness.internal.extension.observeNonNull
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,4 +11,30 @@ class BookmarkWorkoutPlanFragment :
     BaseFragment<BookmarkWorkoutPlanViewModel, FragmentBookmarkWorkoutPlanBinding>() {
     override val layoutId get() = R.layout.fragment_bookmark_workout_plan
 
+    override fun initialize() {
+        super.initialize()
+
+        binding.adapter = BookmarkWorkoutPlanAdapter(object : BookmarkWorkoutPlanCallback {
+            override fun onItemClick() {
+
+            }
+
+            override fun onItemLongClick(): Boolean {
+                viewModel.selecting.value = true
+                return true
+            }
+
+            override fun onItemSelectedChanged(id: Long, isSelected: Boolean) {
+                viewModel.changeItemSelected(id, isSelected)
+            }
+        })
+
+        binding.cancelButton.setOnClickListener {
+            viewModel.selecting.value = false
+        }
+
+        viewModel.selecting.observeNonNull(viewLifecycleOwner) {
+            viewModel.changeItemSelecting(it)
+        }
+    }
 }
--- app/src/main/java/com/ltmb/fitness/scene/bookmarkworkoutplan/BookmarkWorkoutPlanViewModel.kt
+++ app/src/main/java/com/ltmb/fitness/scene/bookmarkworkoutplan/BookmarkWorkoutPlanViewModel.kt
@@ -1,11 +1,67 @@
 package com.ltmb.fitness.scene.bookmarkworkoutplan
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.ltmb.fitness.base.BaseAndroidViewModel
+import com.ltmb.fitness.uimodel.BookmarkWorkoutPlanUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class BookmarkWorkoutPlanViewModel @Inject constructor(
     application: Application
-) : BaseAndroidViewModel(application)
+) : BaseAndroidViewModel(application) {
+
+    private val _bookmarkWorkoutPlans = MutableLiveData<List<BookmarkWorkoutPlanUiModel>>()
+    val bookmarkWorkoutPlans: LiveData<List<BookmarkWorkoutPlanUiModel>> = _bookmarkWorkoutPlans
+    var selecting = MutableLiveData(false)
+    var selected = MutableLiveData(false)
+
+    init {
+        val workoutPlansList = mutableListOf<BookmarkWorkoutPlanUiModel>()
+
+        for (i in 1..10) {
+            val workoutPlan = BookmarkWorkoutPlanUiModel(
+                id = i.toLong(),
+                thumbnail = "https://wallpaperbat.com/img/69222-wallpaper-power-pose-back-fitness-gym-image-for-desktop.jpg",
+                name = "Full Body Workout $i",
+                level = "Intermediate",
+                duration = 60,
+                selecting = false,
+                selected = false
+            )
+            workoutPlansList.add(workoutPlan)
+        }
+
+        _bookmarkWorkoutPlans.value = workoutPlansList
+    }
+
+    fun onAddButtonClick() {}
+
+    fun changeItemSelecting(isSelecting: Boolean) {
+        val workoutPlansList = _bookmarkWorkoutPlans.value!!.toMutableList()
+        for (i in workoutPlansList.indices) {
+            val item = workoutPlansList[i]
+            item.selecting = isSelecting
+            if (!isSelecting) {
+                item.selected = false
+            }
+        }
+        _bookmarkWorkoutPlans.value = workoutPlansList
+    }
+
+    fun changeItemSelected(id: Long, isSelected: Boolean) {
+        val workoutPlansList = _bookmarkWorkoutPlans.value!!.toMutableList()
+        for (i in workoutPlansList.indices) {
+            val item = workoutPlansList[i]
+            if (item.id == id) {
+                item.selected = isSelected
+            }
+        }
+        _bookmarkWorkoutPlans.value = workoutPlansList
+
+        val firstSelectedItem = _bookmarkWorkoutPlans.value?.find { it.selected }
+        selected.value = firstSelectedItem != null
+    }
+}
