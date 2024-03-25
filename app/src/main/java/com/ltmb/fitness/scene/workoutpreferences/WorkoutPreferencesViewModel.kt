80bdbdb57236596c434987d2b4805dec6e6b3980
HongDuy119 <duy1192002@gmail.com>
2024-03-25 07:15:38 +0700

    food

---
+++ app/src/main/java/com/ltmb/fitness/scene/workoutpreferences/WorkoutPreferencesViewModel.kt
@@ -0,0 +1,10 @@
+package com.ltmb.fitness.scene.workoutpreferences
+
+import android.app.Application
+import com.ltmb.fitness.base.BaseAndroidViewModel
+import javax.inject.Inject
+
+class WorkoutPreferencesViewModel @Inject constructor(
+    application: Application,
+) : BaseAndroidViewModel(application) {
+}
