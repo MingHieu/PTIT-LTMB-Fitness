c91146f1bab2dfc4ed068b3fa21e7ff33d27afb8
NhatHoang <lanhathoang@gmail.com>
2024-04-22 00:10:22 +0700

    add line chart day

--- app/src/main/java/com/ltmb/fitness/scene/report/ReportFragment.kt
+++ app/src/main/java/com/ltmb/fitness/scene/report/ReportFragment.kt
@@ -1,6 +1,8 @@
 package com.ltmb.fitness.scene.report
 
+import android.annotation.SuppressLint
 import android.graphics.Color
+import androidx.core.content.ContextCompat
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.charts.PieChart
@@ -18,8 +20,11 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.ltmb.fitness.R
 import com.ltmb.fitness.base.BaseFragment
 import com.ltmb.fitness.databinding.FragmentReportBinding
+import com.ltmb.fitness.internal.extension.observeNonNull
+import com.ltmb.fitness.internal.util.functional.getColorInTheme
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
+
 
 @AndroidEntryPoint
 class ReportFragment : BaseFragment<ReportViewModel, FragmentReportBinding>() {
@@ -29,82 +34,181 @@ class ReportFragment : BaseFragment<ReportViewModel, FragmentReportBinding>() {
     override fun initialize() {
         super.initialize()
 
-        val barChart: BarChart = binding.barChart
+        binding.reportToday.setOnClickListener {
+            viewModel.selectReport.value = true
+        }
 
-        val barDataSet1 = BarDataSet(barEntries(), "Workouts")
+        binding.reportMonth.setOnClickListener {
+            viewModel.selectReport.value = false
+            viewModel.getWorkoutHistoryMonth()
+        }
+
+        viewModel.selectReport.observeNonNull(viewLifecycleOwner){
+            val primaryColor =
+                getColorInTheme(requireContext(), androidx.appcompat.R.attr.colorPrimary)
+
+            val whiteColor = ContextCompat.getColor(requireContext(), R.color.white)
+            val hintColor = ContextCompat.getColor(requireContext(), R.color.alabaster)
+            val textColor = ContextCompat.getColor(requireContext(), R.color.black)
+            if(it){
+                binding.reportToday.setTextColor(primaryColor)
+                binding.reportToday.setBackgroundColor(hintColor)
+                binding.reportMonth.setTextColor(textColor)
+                binding.reportMonth.setBackgroundColor(whiteColor)
+
+            }else{
+                binding.reportMonth.setTextColor(primaryColor)
+                binding.reportMonth.setBackgroundColor(hintColor)
+                binding.reportToday.setTextColor(textColor)
+                binding.reportToday.setBackgroundColor(whiteColor)
+            }
+        }
+
+        val barChart: BarChart = binding.barChart
+        drawBarChart(barChart)
+
+        val lineChart: LineChart = binding.lineChar
+
+
+
+
+        viewModel.getWorkoutHistoryToday()
+
+        viewModel.workoutHistoryList.observeNonNull(viewLifecycleOwner) { workoutHistoryList ->
+            val map = mutableMapOf<Int, Long>()
+            val entries = ArrayList<Entry>()
+            for(wh in workoutHistoryList){
+                val hours = wh.createdAt.hours
+                if(map.containsKey(hours)){
+                    map[hours] = map[hours]!! + wh.times
+                }else{
+                    map[hours] = wh.times
+                }
+               println("Fragment: hours ${wh.createdAt.hours}")
+            }
+            for(i in 0 until 24){
+                if(map.containsKey(i)){
+                    entries.add(Entry(i.toFloat(), map[i]?.toFloat()!!))
+                }
+                else{
+                    entries.add(Entry(i.toFloat(), 0f))
+                }
+            }
+            drawLineChart(lineChart, entries)
+
+        }
+
+//        val lineChart: LineChart = binding.lineChart
+//
+//        val entries = arrayListOf(
+//            Entry(0f, 62f),
+//            Entry(1f, 69f),
+//            Entry(2f, 64f),
+//            Entry(3f, 75f),
+//            Entry(4f, 68f)
+//        )
+//
+//        val dataSet = LineDataSet(entries, "Data Set 1")
+//        dataSet.color = Color.RED
+//        dataSet.setDrawFilled(true)
+//        dataSet.fillColor = Color.BLUE // Màu fill
+//        dataSet.fillAlpha =
+//            128 // Độ trong suốt của fill color (0-255), 255 là hoàn toàn không trong suốt
+//
+//        val lineData = LineData(dataSet)
+//        lineChart.data = lineData
+//        lineChart.invalidate()
+
+//        pieChart()
+
+    }
+
+    private fun drawLineChart(lineChart: LineChart, entries: ArrayList<Entry>){
+
+        val dataSet = LineDataSet(entries, "Thời gian luyện tập trong ngày")
+        dataSet.setColor(Color.BLUE)
+        dataSet.setValueTextColor(Color.BLACK)
+
+//        dataSet.setDrawValues(false)
+
+        val lineData = LineData(dataSet)
+
+        lineChart.setData(lineData)
+
+        lineChart.description.isEnabled = false
+        lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+        lineChart.xAxis.setDrawGridLines(false)
+
+        lineChart.axisLeft.setDrawGridLines(false)
+        lineChart.axisRight.setDrawGridLines(false)
+
+        lineChart.axisLeft.axisMinimum = 0f
+        lineChart.axisRight.axisMinimum = 0f
+
+
+        lineChart.invalidate()
+    }
+
+    private fun barEntries(type: Int): ArrayList<BarEntry> {
+        val barEntries = ArrayList<BarEntry>()
+        val random = Random()
+        var start = 0
+        if(type == 1){
+            start = 5
+        }else if(type == 2){
+            start = 30
+        }else{
+            start = 200
+        }
+        for(i in 0 until 7){
+            barEntries.add(BarEntry(i.toFloat(), (start + random.nextInt(start)).toFloat()))
+        }
+        return barEntries
+    }
+
+
+    private fun drawBarChart(barChart: BarChart){
+        val barDataSet1 = BarDataSet(barEntries(1), "Workout")
         barDataSet1.setColor(Color.RED)
-        val barDataSet2 = BarDataSet(barEntries(), "Minutes")
+        val barDataSet2 = BarDataSet(barEntries(2), "Minute")
         barDataSet2.setColor(Color.GREEN)
-        val barDataSet3 = BarDataSet(barEntries(), "Kcal")
+        val barDataSet3 = BarDataSet(barEntries(3), "Kcal")
         barDataSet3.setColor(Color.YELLOW)
 
         val barData = BarData(barDataSet1, barDataSet2, barDataSet3)
 
         barChart.setData(barData)
 
-        val days = arrayOf("2", "3", "4", "5", "6", "7", "8")
+        val days = arrayOf("2", "3", "4", "5", "6", "7", "CN")
 
         val xAxis: XAxis = barChart.xAxis
         xAxis.valueFormatter = IndexAxisValueFormatter(days)
         xAxis.setCenterAxisLabels(true)
         xAxis.position = XAxis.XAxisPosition.BOTTOM
-        xAxis.setGranularity(1f)
-        xAxis.isGranularityEnabled = true
+//        xAxis.setGranularity(1f)
+//        xAxis.isGranularityEnabled = true
 
-        barChart.setDragEnabled(true)
-        barChart.setVisibleXRangeMaximum(7f)
+//        barChart.setDragEnabled(true)
+//        barChart.setVisibleXRangeMaximum(7f)
 
         val barSpace = 0.08f
         val groupSpace = 0.4f
         barData.barWidth = 0.12f
 
-        barChart.xAxis.setAxisMinimum(0f)
-        barChart.xAxis
-            .setAxisMaximum(0 + barChart.barData.getGroupWidth(groupSpace, barSpace) * 7)
+        barChart.description.isEnabled = false
+
+        xAxis.axisMinimum = 0f
+        xAxis.setAxisMaximum(0 + barChart.barData.getGroupWidth(groupSpace, barSpace) * 7)
         barChart.axisLeft.setAxisMinimum(0f)
+        barChart.axisRight.setAxisMinimum(0f)
+
+        barChart.xAxis.setDrawGridLines(false)
 
         barChart.groupBars(0f, groupSpace, barSpace)
 
-        barChart.setDrawGridBackground(false)
 
         barChart.invalidate()
-
-        val lineChart: LineChart = binding.lineChart
-
-        val entries = arrayListOf(
-            Entry(0f, 62f),
-            Entry(1f, 69f),
-            Entry(2f, 64f),
-            Entry(3f, 75f),
-            Entry(4f, 68f)
-        )
-
-        val dataSet = LineDataSet(entries, "Data Set 1")
-        dataSet.color = Color.RED
-        dataSet.setDrawFilled(true)
-        dataSet.fillColor = Color.BLUE // Màu fill
-        dataSet.fillAlpha =
-            128 // Độ trong suốt của fill color (0-255), 255 là hoàn toàn không trong suốt
-
-        val lineData = LineData(dataSet)
-        lineChart.data = lineData
-        lineChart.invalidate()
-
-        pieChart()
-
-    }
-
-    private fun barEntries(): ArrayList<BarEntry> {
-        val barEntries = ArrayList<BarEntry>()
-        val random = Random()
-        barEntries.add(BarEntry(1f, (200 + random.nextInt(300)).toFloat()))
-        barEntries.add(BarEntry(2f, (200 + random.nextInt(300)).toFloat()))
-        barEntries.add(BarEntry(3f, (200 + random.nextInt(300)).toFloat()))
-        barEntries.add(BarEntry(4f, (200 + random.nextInt(300)).toFloat()))
-        barEntries.add(BarEntry(5f, (200 + random.nextInt(300)).toFloat()))
-        barEntries.add(BarEntry(6f, (200 + random.nextInt(300)).toFloat()))
-        barEntries.add(BarEntry(7f, (200 + random.nextInt(300)).toFloat()))
-        return barEntries
     }
 
     fun pieChart() {
--- app/src/main/java/com/ltmb/fitness/scene/report/ReportViewModel.kt
+++ app/src/main/java/com/ltmb/fitness/scene/report/ReportViewModel.kt
@@ -1,10 +1,75 @@
 package com.ltmb.fitness.scene.report
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.ltmb.fitness.base.BaseAndroidViewModel
+import com.ltmb.fitness.data.remote.model.workouthistory.WorkoutHistoryModel
+import com.ltmb.fitness.data.repository.WorkoutHistoryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.Calendar
+import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
-class ReportViewModel @Inject constructor(application: Application) :
-    BaseAndroidViewModel(application)
+class ReportViewModel @Inject constructor(
+    application: Application,
+    private val workoutHistoryRepository: WorkoutHistoryRepository
+) : BaseAndroidViewModel(application) {
+
+    private val _workoutHistoryList = MutableLiveData<List<WorkoutHistoryModel>>()
+    val workoutHistoryList: LiveData<List<WorkoutHistoryModel>> = _workoutHistoryList
+    val selectReport = MutableLiveData(true)
+
+    private fun setDateTime(date: Date) : Calendar {
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        return calendar
+    }
+
+    private fun startToday(): Date {
+        val calendar = setDateTime(Date())
+        return calendar.time
+    }
+    private fun startTomorrow(today: Date): Date {
+        val calendar = Calendar.getInstance()
+        calendar.time = today
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        return calendar.time
+    }
+    private fun startDayOfMonth(): Date {
+        val calendar = setDateTime(Date())
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
+        return calendar.time
+    }
+
+    private fun startDayOfNextMonth(startDayOfMonth: Date): Date {
+        val calendar = Calendar.getInstance()
+        calendar.time = startDayOfMonth
+        calendar.add(Calendar.MONTH, 1)
+        return calendar.time
+    }
+
+
+    fun getWorkoutHistoryToday() {
+        val today = startToday()
+        val tomorrow = startTomorrow(today)
+        viewModelScope.launch {
+            _workoutHistoryList.value = workoutHistoryRepository.getWorkoutHistory(today, tomorrow)
+        }
+    }
+
+    fun getWorkoutHistoryMonth() {
+        val startDayOfMonth = startDayOfMonth()
+        val startDayOfNextMonth = startDayOfNextMonth(startDayOfMonth)
+        println("--------------------ViewModel: ${startDayOfMonth}")
+        println("--------------------ViewModel: ${startDayOfNextMonth}")
+    }
+
+}
